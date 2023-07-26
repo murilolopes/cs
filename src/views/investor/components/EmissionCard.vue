a32dcd2e46a7ca242550e47bb5f27b8507f7416b
Murilo Angelo <muriloangelo10@gmail.com>
2023-07-26 10:12:55 -0300

    emission card component and favorite actions draft

---
+++ src/views/investor/components/EmissionCard.vue
@@ -0,0 +1,143 @@
+<template>
+  <b-card class="border-light">
+    <div class="emissao-card-header d-flex justify-content-between align-items-end mb-2">
+      <b-img :src="emissaoImage(emission)" class="mr-1" />
+      <div class="d-flex flex-column justify-content-start">
+        <h4>
+          {{ emissionTitle(emission) }}
+        </h4>
+        <div class="d-flex justify-content-between">
+          <span>{{ emission.valor_global_emissao | moneyFormat }}</span>
+          <feather-icon
+            @click="favoriteEmission(emission)"
+            icon="HeartIcon"
+            size="20"
+            class="text-secondary"
+            :class="{ 'filled-heart': emission.favorited }"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="emissao-card-data">
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> Status </span>
+        <b-badge
+          :class="`badge badge-light-${emission.is_ativo ? 'success' : 'danger'} border-${
+            emission.is_ativo ? 'success' : 'danger'
+          }`"
+        >
+          <span class="text-dark">
+            {{ emission.is_ativo ? 'Ativo' : 'Inativo' }}
+          </span>
+        </b-badge>
+      </div>
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> Emissão </span>
+        <strong> {{ emission.numero_emissao }} </strong>
+      </div>
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> Data da Emissão </span>
+        <strong> {{ emission.data_emissao | formatDate }} </strong>
+      </div>
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> Data do Vencimento </span>
+        <strong> {{ emission.data_vencimento | formatDate }} </strong>
+      </div>
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> Código IF B3 </span>
+        <strong> {{ emission.id }} </strong>
+      </div>
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> ISIN </span>
+        <strong> {{ emission.codigo_isin }} </strong>
+      </div>
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> Emissor </span>
+        <strong> {{ emission.emissor }} </strong>
+      </div>
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> Agente Fiduciário </span>
+        <strong> {{ emission.agente_fiduciario }} </strong>
+      </div>
+      <div class="d-flex justify-content-between border-top pt-50 pb-50">
+        <span> Remuneração </span>
+        <strong> {{ emission.remuneracao }} </strong>
+      </div>
+    </div>
+    <div class="emissao-card-action">
+      <b-button
+        variant="primary"
+        class="emissoes-details-button w-100 border-primary"
+        @click="$router.push({ name: 'detalhes', params: { id: emission.id } })"
+      >
+        <span class="text-dark d-flex justify-content-center align-items-center font-weight-bolder">
+          Detalhes
+          <feather-icon icon="ArrowRightIcon" size="20" class="ml-1" />
+        </span>
+      </b-button>
+    </div>
+  </b-card>
+</template>
+
+<script>
+import { BCard, BImg, BBadge, BButton } from 'bootstrap-vue'
+import { moneyFormat } from '@/@core/comp-functions/data_visualization/datatable'
+import { formatDate } from '@core/utils/filter'
+import { mask } from 'vue-the-mask'
+
+export default {
+  name: 'EmissionCard',
+  components: {
+    BCard,
+    BImg,
+    BBadge,
+    BButton,
+  },
+  props: {
+    emission: {
+      type: Object,
+      required: true,
+    },
+  },
+  directives: { mask },
+  filters: {
+    moneyFormat,
+    formatDate,
+  },
+  methods: {
+    emissionTitle(emission) {
+      let title = `${emission.tipo_emissao} - ${emission.nome_fantasia} - ${emission.tipo_suboordinacao}`
+      const truncatedTitle = title.substring(0, 20)
+
+      return title.length > 20 ? `${truncatedTitle}...` : truncatedTitle
+    },
+    emissaoImage(emissao) {
+      const options = {
+        cri: 'cri',
+        cra: 'cra',
+      }
+
+      if (!options[emissao.tipo_emissao.toLowerCase()])
+        return require('@/assets/images/recebíveis.svg')
+
+      return require(`@/assets/images/${options[emissao.tipo_emissao.toLowerCase()]}.svg`)
+    },
+    favoriteEmission(emission) {
+      try {
+        // this.$store.dispatch('emissions/favoriteEmission', emission)
+        // this.$emit('favoriteEmission', emission)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import '~@/assets/scss/variables/_variables.scss';
+
+.filled-heart {
+  fill: $secondary;
+}
+</style>
--- src/views/investor/pages/Emissoes.vue
+++ src/views/investor/pages/Emissoes.vue
@@ -1,194 +1,128 @@
 <template>
-  <div>
-    <b-card>
-      <b-row class="d-flex flex-row justify-content-between align-items-center">
-        <b-col sm="12" md="6" lg="4" xl="4">
-          <b-input-group class="input-group-merge mb-2">
-            <b-input-group-prepend class="cs-select">
-              <v-select v-model="search" :options="searchOptions" :clearable="false" />
-            </b-input-group-prepend>
-            <b-form-input
-              id="x1"
-              placeholder="Buscar"
-              class="px-1"
-              v-mask="'##/####'"
-              @keypress.enter="updatePayload($event.target.value)"
-              v-if="search.value === 'mes_emissao' || search.value === 'mes_vencimento'"
-            />
-            <b-form-input
-              id="x2"
-              placeholder="Buscar"
-              class="px-1"
-              v-mask="''"
-              @keypress.enter="updatePayload($event.target.value)"
-              v-else
-            />
-            <b-input-group-append is-text>
-              <feather-icon icon="SearchIcon" />
-            </b-input-group-append>
-          </b-input-group>
-        </b-col>
-        <b-col sm="12" md="6" lg="4" xl="4">
-          <b-button-group class="mb-2 text-nowrap mr-3">
-            <b-button
-              :variant="selectedButtonVariant.all"
-              class="text-dark"
-              @click="selectButton('all')"
-            >
-              <strong>Todos</strong>
-            </b-button>
-            <b-button
-              :variant="selectedButtonVariant.cri"
-              class="text-dark"
-              @click="selectButton('cri')"
-            >
-              <strong>CRI</strong>
-            </b-button>
-            <b-button
-              :variant="selectedButtonVariant.cra"
-              class="text-dark"
-              @click="selectButton('cra')"
-            >
-              <strong>CRA</strong>
-            </b-button>
-            <b-button
-              :variant="selectedButtonVariant.in_progress"
-              class="text-dark"
-              @click="selectButton('in_progress')"
-            >
-              <strong>Em andamento</strong>
-            </b-button>
-          </b-button-group>
-        </b-col>
-        <b-col
-          class="d-flex flex-row justify-content-end align-items-center mb-2"
-          sm="12"
-          md="12"
-          lg="4"
-          xl="4"
-        >
-          <label class="mb-0 mr-1">Mostrar</label>
-          <v-select v-model="perPage" :options="perPageOptions" :clearable="false" />
-          <label class="mr-2 ml-1">registros</label>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col sm="6" md="6" lg="6" xl="4" v-for="(item, index) in items" :key="index">
-          <b-card>
-            <div class="emissao-card-header d-flex align-items-end mb-2">
-              <b-img :src="emissaoImage(item)" class="mr-1" />
-              <div class="d-flex flex-column justify-content-start">
-                <h4>
-                  {{ item.tipo_emissao }} - {{ item.nome_fantasia }} -
-                  {{ item.tipo_suboordinacao }}
-                </h4>
-                <span>{{ item.valor_global_emissao | moneyFormat }}</span>
-              </div>
-            </div>
-            <div class="emissao-card-data">
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> Status </span>
-                <b-badge
-                  :class="`badge badge-light-${item.is_ativo ? 'success' : 'danger'} border-${
-                    item.is_ativo ? 'success' : 'danger'
-                  }`"
-                >
-                  <span class="text-dark">
-                    {{ item.is_ativo ? 'Ativo' : 'Inativo' }}
-                  </span>
-                </b-badge>
-              </div>
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> Emissão </span>
-                <strong> {{ item.numero_emissao }} </strong>
-              </div>
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> Data da Emissão </span>
-                <strong> {{ item.data_emissao | formatDate }} </strong>
-              </div>
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> Data do Vencimento </span>
-                <strong> {{ item.data_vencimento | formatDate }} </strong>
-              </div>
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> Código IF B3 </span>
-                <strong> {{ item.id }} </strong>
-              </div>
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> ISIN </span>
-                <strong> {{ item.codigo_isin }} </strong>
-              </div>
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> Emissor </span>
-                <strong> {{ item.emissor }} </strong>
-              </div>
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> Agente Fiduciário </span>
-                <strong> {{ item.agente_fiduciario }} </strong>
-              </div>
-              <div class="d-flex justify-content-between border-top pt-50 pb-50">
-                <span> Remuneração </span>
-                <strong> {{ item.remuneracao }} </strong>
-              </div>
-            </div>
-            <div class="emissao-card-action">
-              <b-button
-                variant="primary"
-                class="emissoes-details-button w-100 border-primary"
-                @click="$router.push({ name: 'detalhes', params: { id: item.id } })"
-              >
-                <span
-                  class="text-dark d-flex justify-content-center align-items-center font-weight-bolder"
-                >
-                  Detalhes
-                  <feather-icon icon="ArrowRightIcon" size="20" class="ml-1" />
-                </span>
-              </b-button>
-            </div>
-          </b-card>
-        </b-col>
-      </b-row>
-      <b-row class="mt-2">
-        <b-col
-          cols="12"
-          sm="6"
-          class="d-flex align-items-center justify-content-center justify-content-sm-start"
-        >
-          <span class="text-muted"> {{ showing }} </span>
-        </b-col>
-        <b-col
-          cols="12"
-          sm="6"
-          class="d-flex align-items-center justify-content-center justify-content-sm-end"
-        >
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="totalRows"
-            :per-page="perPage"
-            first-number
-            last-number
-            prev-class="prev-item"
-            next-class="next-item"
-            class="mb-0"
-            aria-controls="my-table"
+  <b-card>
+    <b-row class="d-flex flex-row justify-content-between align-items-center">
+      <b-col sm="12" md="6" lg="4" xl="4">
+        <b-input-group class="input-group-merge mb-2">
+          <b-input-group-prepend class="cs-select">
+            <v-select v-model="search" :options="searchOptions" :clearable="false" />
+          </b-input-group-prepend>
+          <b-form-input
+            id="x1"
+            placeholder="Buscar"
+            class="px-1"
+            v-mask="'##/####'"
+            @keypress.enter="updatePayload($event.target.value)"
+            v-if="search.value === 'mes_emissao' || search.value === 'mes_vencimento'"
+          />
+          <b-form-input
+            id="x2"
+            placeholder="Buscar"
+            class="px-1"
+            v-mask="''"
+            @keypress.enter="updatePayload($event.target.value)"
+            v-else
+          />
+          <b-input-group-append is-text>
+            <feather-icon icon="SearchIcon" />
+          </b-input-group-append>
+        </b-input-group>
+      </b-col>
+      <b-col sm="12" md="6" lg="4" xl="4">
+        <b-button-group class="mb-2 text-nowrap mr-3">
+          <b-button
+            :variant="selectedButtonVariant.all"
+            class="text-dark"
+            @click="selectButton('all')"
           >
-            <template #prev-text>
-              <feather-icon icon="ChevronLeftIcon" size="18" />
-            </template>
-            <template #next-text>
-              <feather-icon icon="ChevronRightIcon" size="18" />
-            </template>
-          </b-pagination>
-        </b-col>
-      </b-row>
-    </b-card>
-  </div>
+            <strong>Todos</strong>
+          </b-button>
+          <b-button
+            :variant="selectedButtonVariant.cri"
+            class="text-dark"
+            @click="selectButton('cri')"
+          >
+            <strong>CRI</strong>
+          </b-button>
+          <b-button
+            :variant="selectedButtonVariant.cra"
+            class="text-dark"
+            @click="selectButton('cra')"
+          >
+            <strong>CRA</strong>
+          </b-button>
+          <b-button
+            :variant="selectedButtonVariant.favorited"
+            class="text-dark"
+            @click="selectButton('favorited')"
+          >
+            <strong>Favoritos</strong>
+          </b-button>
+          <b-button
+            :variant="selectedButtonVariant.in_progress"
+            class="text-dark"
+            @click="selectButton('in_progress')"
+          >
+            <strong>Em andamento</strong>
+          </b-button>
+        </b-button-group>
+      </b-col>
+      <b-col
+        class="d-flex flex-row justify-content-end align-items-center mb-2"
+        sm="12"
+        md="12"
+        lg="4"
+        xl="4"
+      >
+        <label class="mb-0 mr-1">Mostrar</label>
+        <v-select v-model="perPage" :options="perPageOptions" :clearable="false" />
+        <label class="mr-2 ml-1">registros</label>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col sm="6" md="6" lg="6" xl="4" v-for="(item, index) in items" :key="index">
+        <emission-card :emission="item" />
+      </b-col>
+    </b-row>
+    <b-row class="mt-2">
+      <b-col
+        cols="12"
+        sm="6"
+        class="d-flex align-items-center justify-content-center justify-content-sm-start"
+      >
+        <span class="text-muted"> {{ showing }} </span>
+      </b-col>
+      <b-col
+        cols="12"
+        sm="6"
+        class="d-flex align-items-center justify-content-center justify-content-sm-end"
+      >
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="totalRows"
+          :per-page="perPage"
+          first-number
+          last-number
+          prev-class="prev-item"
+          next-class="next-item"
+          class="mb-0"
+          aria-controls="my-table"
+        >
+          <template #prev-text>
+            <feather-icon icon="ChevronLeftIcon" size="18" />
+          </template>
+          <template #next-text>
+            <feather-icon icon="ChevronRightIcon" size="18" />
+          </template>
+        </b-pagination>
+      </b-col>
+    </b-row>
+  </b-card>
 </template>
 
 <script>
 import { moneyFormat } from '@/@core/comp-functions/data_visualization/datatable'
-// eslint-disable-next-line import/no-cycle
 import { formatDate } from '@core/utils/filter'
+import EmissionCard from '@/views/investor/components/EmissionCard.vue'
 import {
   BRow,
   BCol,
@@ -222,6 +156,7 @@ export default {
     vSelect,
     BFormInput,
     BPagination,
+    EmissionCard,
   },
   directives: { mask },
   filters: {
@@ -298,19 +233,6 @@ export default {
         console.log(error)
       }
     },
-    emissaoImage(emissao) {
-      const options = {
-        cri: 'cri',
-        cra: 'cra',
-      }
-
-      // eslint-disable-next-line global-require, import/no-unresolved
-      if (!options[emissao.tipo_emissao.toLowerCase()])
-        return require('@/assets/images/recebíveis.svg')
-
-      // eslint-disable-next-line import/no-dynamic-require, global-require
-      return require(`@/assets/images/${options[emissao.tipo_emissao.toLowerCase()]}.svg`)
-    },
     selectButton(type) {
       this.selectedButton = type
     },
@@ -338,6 +260,7 @@ export default {
         all: 'outline-primary',
         cri: 'outline-primary',
         cra: 'outline-primary',
+        favorited: 'outline-primary',
         in_progress: 'outline-primary',
       }
 
